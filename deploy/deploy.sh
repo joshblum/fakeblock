9bb899192eb8f953763ae1a20da0f862df6a169b
Joshua Blum <joshblum@mit.edu>
2013-08-03 13:13:19 -0400

    structure

--- deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#make baseUrl production url, update the manifest count
-python deploy/deploy.py
-
-#remove old zip and add new one
-cd ../
-rm chrome-ext.zip
-zip -r chrome-ext.zip chrome-ext/
--- js/main.js
+++
@@ -1,4 +0,0 @@
-///////////Global vars/////////////
-// global website base, set to localhost for testing, use deploy script to change
-var baseUrl = "http://localhost:5000";
-// var baseUrl = "http://fakeblock.herokuapp.com";
