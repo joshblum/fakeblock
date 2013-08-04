74c0b4a935578d42f0e52e4ad77ea6827d4fbf2a
Joshua Blum <joshblum@mit.edu>
2013-08-04 02:57:17 -0400

    ahppyyyyy

--- chrome-ext/js/background/decrypt.js
+++ chrome-ext/js/background/decrypt.js
@@ -1,7 +1,6 @@
 //returns a plaintext result of the decryption
 //or an empty string if decryption fails.
 function decrypt(fakeblock) {
-    debugger
     var user_meta = loadLocalStore('user_meta');
     if (!Object.size(user_meta)){
         return ""
@@ -16,8 +15,8 @@ function decrypt(fakeblock) {
     var e_shared_secrets = encrypted_data.e_shared_secrets;
     var priv_key = deserializePrivKey(user_meta.priv_key);
     for (var i = 0; i < e_sentinals.length; i++) {
-        if (cryptico.decrypt(e_sentinals[i], priv_key) === SENTINAL) {
-            var shared_secret = cryptico.decrypt(e_shared_secrets[i], user_meta.priv_key);
+        if (cryptico.decrypt(e_sentinals[i], priv_key).plaintext === SENTINAL) {
+            var shared_secret = cryptico.decrypt(e_shared_secrets[i], priv_key).plaintext;
             return CryptoJS.AES.decrypt(Base64.decode(fakeblock.cipher_text), shared_secret).toString(CryptoJS.enc.Utf8);
         }
     }
