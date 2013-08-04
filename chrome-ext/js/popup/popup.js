765f733721ac9f628be2faa04257da46248fbbb1
Max Fowler <max_fowler@brown.edu>
2013-08-04 03:05:53 -0400

    frontend thang

--- chrome-ext/js/popup/popup.js
+++ chrome-ext/js/popup/popup.js
@@ -1,24 +1,29 @@
 var userInputs = [];
 
-$(function() {
-	sendMessage({
-		"action" : "get_friends",
-	}, function(friends) {
-		for (friend in friends) {
-			var userInput = $('<div class="contact">
-	            <input class="select-contact" type="checkbox"/>
-	            <div class="contact-name">
-	            ' + friends.friend.name + '
-	            </div>
-	        </div>');
-	        userInput.data('username', friend);
-	        userInput.data('fullname', friends.friend.name);
-	        userInputs.push(userInput);
-	        $('body').append(userInput);
-		}
-	});
+//$(function() {
+//	sendMessage({
+//		"action" : "get_friends",
+//	}, function(friends) {
+//		for (friend in friends) {
+//			var userInput = $('<div class="contact">
+//	            <input class="select-contact" type="checkbox"/>
+//	            <div class="contact-name">
+//	            ' + friends.friend.name + '
+//	            </div>
+//	        </div>');
+//	        userInput.data('username', friend);
+//	        userInput.data('fullname', friends.friend.name);
+//	        userInputs.push(userInput);
+//	        $('body').append(userInput);
+//		}
+//	});
+//
+//
+//});
 
-    $(".contact").click(function() {
+
+$(document).ready(function() {
+  $(".contact").click(function() {
         if ($(this).hasClass("clicked")) {
             $(this).removeClass("clicked");
             $(this).find("input").prop('checked', false);
