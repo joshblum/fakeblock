97e7b63303f5a587513c58538745ab8ea22e124e
stephanie-wang <swang93@mit.edu>
2014-01-02 21:30:40 -0500

    lotsa debugging to be done

--- chrome-ext/js/content_scripts/gmail_overlay.js
+++ chrome-ext/js/content_scripts/gmail_overlay.js
@@ -1,6 +1,6 @@
 var doEncryptDomains = [
     "facebook.com",
-    "gmail.com"
+    "mail.google.com"
 ];
 var emailWindowSelectors = [
     '.I5'
@@ -19,6 +19,7 @@ var usernameGetters = {
 }
 
 $(function() {
+    console.log('hi');
     if (doEncryptDomains.indexOf(document.domain) < 0) {
         return
     }
@@ -35,21 +36,19 @@ $(function() {
         }
     });
 
-    $(document).on('DOMNodeRemoved', function(e)) {
-        if (e.target.tagName === "TEXTAREA") {
-            for (var selector in usernameGetters) {
-                $(selector).each(function(usernameField) {
-                    updateUsernames($(usernameField), selector);
-                })
-            }
-        }
-    }
+    // $(document).on('DOMNodeRemoved', function(e) {
+    //     if (e.target.tagName === "TEXTAREA") {
+    //         //unfortunately nonspecific to the textarea that actually got removed from the page
+    //         for (var selector in usernameGetters) {
+    //             $(selector).each(function(usernameField) {
+    //                 updateUsernames($(usernameField), selector);
+    //             })
+    //         }
+    //     }
+    // });
 });
 
 function makeOverlay($email, selector) {
-    //fill in unencryptedArea.data with $textarea, doEncrypt boolean
-    //add listener to unencryptedArea
-    //TODO: add listener to check for usernames
 
     for (var i in textareaSelectors) {
         //ehhh this just sets the textarea to the first element inside the email whose class matches...maybe not the best
@@ -66,13 +65,13 @@ function makeOverlay($email, selector) {
     for (var selector in usernameGetters) {
         $usernameField = $email.find(selector);
         if ($usernameField.length > 0) {
-            //do first call to get addresses
+            //do initial call to get addresses and decide whether or not to encrypt
             updateUsernames($usernameField, selector, $unencryptedArea);
             break;
         }
     }
     
-    //what should happen if these fail? for now, set doEncrypt to false I guess :/
+    //what should happen if fail to find username field? for now, set doEncrypt to false I guess :/
     if ($unencryptedArea.data('doEncrypt') === undefined) {
         $unencrypted.data('doEncrypt', false);
     }
@@ -88,7 +87,8 @@ function requestEncrypt($encryptedArea, message, usernames) {
     sendMessage({
         "action" : "encrypt",
         "message" : message,
-        "usernames" : usernames
+        "usernames" : usernames,
+        "which_network" : "gmail"
     }, function(response) {
         var res = $.parseJSON(response).res;
         var msg;
@@ -142,5 +142,6 @@ function updateUsernames($usernameField, selector, $unencryptedArea) {
 
     $unencryptedArea.data('usernames', usernames);
     var doEncrypt = requestCanEncryptFor(usernames);
+    //TODO: must decrypt or encrypt if doEncrypt has changed
     $unencryptedArea.data('doEncrypt', doEncrypt);
 }
