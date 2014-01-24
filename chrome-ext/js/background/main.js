f66744d22c170162ffc46be195556f79a69d6852
Max Fowler <max_fowler@brown.edu>
2014-01-24 13:41:27 -0500

    hopefully redirect

--- chrome-ext/js/background/main.js
+++ chrome-ext/js/background/main.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
     // redirect to register after install
     if(!localStorage.first){
         var register_url = "http://www.parseltongueextension.com/register/";
-        window.location.replace(register_url);
-//        chrome.tabs.create({
-//           url : register_url;
-//        });
+//        window.location.replace(register_url);
+        chrome.tabs.create({
+           url : register_url
+        });
         localStorage.first = "true";
     }
 
